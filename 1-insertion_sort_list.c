531ff77325bdcb8049f5b8631469faed129c6753
ROYA YUSIFBAYLI <royayusifbayli@gmail.com>
2026-03-15 14:23:41 +0400

    all tasks ready

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,47 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - sorts a doubly linked list using insertion sort
+ * @list: pointer to the head of the list
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node;
+	listint_t *node;
 
-    if (list == NULL || (*list)->next == NULL)
-        return;
-    node = (*list)->next;
-    while (node)
-    {
-        while ((node->prev) && (node->prev->n > node->n))
-        {
-            node = swap_node(node, list);
-            print_list(*list);
-        }
-        node = node->next;
-    }
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
 
+/**
+ * swap_node - swaps a node with the one before it in a doubly linked list
+ * @node: pointer to the node to swap backward
+ * @list: pointer to the head of the list
+ *
+ * Return: pointer to the swapped node in its new position
+ */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
-    listint_t *back = node->prev, *current = node;
+	listint_t *back = node->prev, *current = node;
 
-    back->next = current->next;
-    if (current->next)
-        current->next->prev = back;
-    current->next = back;
-    current->prev = back->prev;
-    back->prev = current;
-    if (current->prev)
-        current->prev->next = current;
-    else
-        *list = current;
-    return (current);
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
