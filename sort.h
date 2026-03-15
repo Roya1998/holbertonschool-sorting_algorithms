b76f06d46554a8b25a4bd1cc6012358f7968e3dd
ROYA YUSIFBAYLI <royayusifbayli@gmail.com>
2026-03-15 13:48:03 +0400

    all tasks ready

--- sort.h
+++ sort.h
@@ -3,11 +3,17 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s - doubly linked list node
+ * @n: integer stored in the node
+ * @prev: pointer to previous node
+ * @next: pointer to next node
+ */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
